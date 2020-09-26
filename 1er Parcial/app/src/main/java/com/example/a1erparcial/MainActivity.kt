2e349f25ce207d7d78122128800a9849e4c06c8e
DionelShiftHA <dionel.marmori@shiftha.com>
2020-09-26 15:52:00 -0300

    comit

--- 1er Parcial/app/src/main/java/com/example/a1erparcial/MainActivity.kt
+++ 1er Parcial/app/src/main/java/com/example/a1erparcial/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.a1erparcial
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnIngresar = findViewById<Button>(  R.id.btnIngresar)
+
+        btnIngresar.setOnClickListener{
+            val intento1 = Intent(this, Menu::class.java)
+            startActivity(intento1)
+        }
+
+
     }
 }
