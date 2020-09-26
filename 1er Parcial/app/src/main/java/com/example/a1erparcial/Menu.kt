40d672dfa2ee328f986eb762601c50cf35dd2b08
DionelShiftHA <dionel.marmori@shiftha.com>
2020-09-26 16:11:55 -0300

    comit2

--- 1er Parcial/app/src/main/java/com/example/a1erparcial/Menu.kt
+++ 1er Parcial/app/src/main/java/com/example/a1erparcial/Menu.kt
@@ -2,11 +2,27 @@ package com.example.a1erparcial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class Menu : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        val btnVerificar = findViewById<Button>(  R.id.btnVerificar)
+
+        btnVerificar.setOnClickListener{
+            if (ControlDeConexion.hayConexion(this))
+            {
+                Toast.makeText(this,"¡¡¡Hay Conexion!!!", Toast.LENGTH_SHORT).show()
+
+            }else
+            {
+                Toast.makeText(this,"¡¡¡Sin Conexión!!!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
